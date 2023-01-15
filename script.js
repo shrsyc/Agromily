9700f9e7bff7e3a2f7df3251e0ed248369049289
shrsyc <shrsyc@gmail.com>
2023-01-15 11:28:06 +0530

    smooth scrolling to link

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ else{
 nav.removeAttribute("style");
 }
 }
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
